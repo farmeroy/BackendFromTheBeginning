27cccec40d27a1e04bd33db51b646dfd3e92068e
Raffaele Cataldo <kemanicataldo@gmail.com>
2026-08-25 13:02:48 -0700

    feat: parse request

--- shttp/shttp.go
+++ shttp/shttp.go
@@ -3,25 +3,29 @@
 package shttp
 
 import (
+	"bytes"
+	"encoding"
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strings"
 )
 
-type Header struct{Key, Value string}
+type Header struct{ Key, Value string }
 
 type Request struct {
-	Method string
-	Path string
+	Method  string
+	Path    string
 	Headers []Header
-	Body string
+	Body    string
 }
 
 type Response struct {
 	StatusCode int
-	Headers []Header
-	Body string
+	Headers    []Header
+	Body       string
 }
 
 func NewRequest(method, path, host, body string) (*Request, error) {
@@ -56,8 +60,8 @@ func NewResponse(status int, body string) (*Response, error) {
 		headers[0] = Header{"Content-Length", fmt.Sprintf("%d", len(body))}
 		return &Response{
 			StatusCode: status,
-			Headers: headers,
-			Body: body,
+			Headers:    headers,
+			Body:       body,
 		}, nil
 	}
 }
@@ -97,11 +101,11 @@ func newTitleCase(key string) string {
 
 // straight from K&R C, 2nd edition, page 43. some classics never go out of style.
 func lower(c byte) byte {
-    /* if you're having trouble understanding this:
-        the idea is as follows: A..=Z are 65..=90, and a..=z are 97..=122.
-        so upper-case letters are 32 less than their lower-case counterparts (or 'a'-'A' == 32).
-        rather than using the 'magic' number 32, we use 'a'-'A' to get the same result.
-    */
+	/* if you're having trouble understanding this:
+	   the idea is as follows: A..=Z are 65..=90, and a..=z are 97..=122.
+	   so upper-case letters are 32 less than their lower-case counterparts (or 'a'-'A' == 32).
+	   rather than using the 'magic' number 32, we use 'a'-'A' to get the same result.
+	*/
 	if c >= 'A' && c <= 'Z' {
 		return c + 'a' - 'A'
 	}
@@ -116,7 +120,7 @@ func upper(c byte) byte {
 }
 
 func isTitleCase(key string) bool {
-	for i:= range key {
+	for i := range key {
 		if i == 0 || key[i-1] == '-' {
 			if key[i] >= 'a' && key[i] <= 'z' {
 				return false
@@ -126,4 +130,152 @@ func isTitleCase(key string) bool {
 		}
 	}
 	return true
+}
+
+// write the request to the given io.Writer
+func (r *Request) WriteTo(w io.Writer) (n int64, err error) {
+	// write & count bytes written
+	// using small closuers like this to cut down on repition
+	// can be nice but sometiems thee is a performance penalty
+	printf := func(format string, args ...any) error {
+		m, err := fmt.Fprintf(w, format, args...)
+		n += int64(m)
+		return err
+	}
+	// remember, a HTTP request looks like this:
+	// <METHOD>  <PATH>  <PROTOCOL/VERSION>
+	// <HEADER>: <VALUE>
+	// <HEADER>: <VALUE>
+	//
+	// <REQUEST BODY>
+
+	// write the request line: like "GET /index.html HTTP/1.1"
+
+	if err := printf("%s: %s\r\n", r.Method, r.Path); err != nil {
+		return n, err
+	}
+
+	// write the headers. we don't bother here to combine them or merge duplicated headers, this is a very basic implementation
+	for _, h := range r.Headers {
+		if err := printf("%s: %s\r\n", h.Key, h.Value); err != nil {
+			return n, err
+		}
+	}
+	printf("\r\n")                 // there must be an empty line between headers and body
+	err = printf("%s\r\n", r.Body) // write body and terminate with newline
+	return n, err
+}
+
+func (resp *Response) WriteTo(w io.Writer) (n int64, err error) {
+	printf := func(format string, args ...any) error {
+		m, err := fmt.Fprintf(w, format, args...)
+		n += int64(m)
+		return err
+	}
+	if err := printf("HTTP/1.1 %d %s\r\n", resp.StatusCode, http.StatusText(resp.StatusCode)); err != nil {
+		return n, err
+	}
+	for _, h := range resp.Headers {
+		if err := printf("%s: %s\r\n", h.Key, h.Value); err != nil {
+			return n, err
+		}
+	}
+	printf("\r\n")
+	if err := printf("%s\r\n", resp.Body); err != nil {
+		return n, err
+	}
+	return n, err
+}
+
+// We can improve our library by writing implementations of std library interfaces
+
+var _, _ fmt.Stringer = (*Request)(nil), (*Response)(nil) // compile time check
+
+var _, _ encoding.TextMarshaler = (*Request)(nil), (*Response)(nil)
+
+func (r *Request) String() string { 
+	b:= new(strings.Builder)
+	r.WriteTo(b) // we take advantage of our WriteTo implementation
+	return b.String()
+}
+
+func (resp *Response) String() string {
+	b := new(strings.Builder)
+	resp.WriteTo(b)
+	return b.String()
+}
+
+// TextMarshaler is used to get a bylte slice representation of a type in order to serialize it across the network or to disk
+func (r *Request) MarshalText() ([]byte, error) {
+	b := new(bytes.Buffer)
+	r.WriteTo(b)
+	return b.Bytes(), nil
+}
+
+func (resp *Response) MarshalText() ([]byte, error) {
+	b := new(bytes.Buffer)
+	resp.WriteTo(b)
+	return b.Bytes(), nil
+}
+
+func splitLines(s string) []string {
+	if s == "" {
+		return nil
+	}
+	var lines []string
+	i := 0
+	for {
+		j := strings.Index(s[i:], "\r\n")
+		if j == -1 {
+			lines = append(lines, s[i:])
+			return lines
+		}
+		lines = append(lines, s[i:i+j]) // up to but not includeing the \r\n
+		i += j + 2 // skip the \r\n
+	}
+}
+
+// ParseRequest parses a HTTP request from given Text
+func ParseRequest(raw string) (r Request, err error) {
+	// request has three parts:
+	// 1. request line
+	// 2. headers
+	// 3. body (optional)
+	lines := splitLines(raw)
+
+	log.Println(lines)
+	if len(lines) < 3 {
+		return Request{}, fmt.Errorf("malformed request: should have at least 3 lines")
+	}
+	first := strings.Fields(lines[0])
+	r.Method, r.Path = first[0], first[1]
+	if !strings.HasPrefix(r.Path, "/") {
+		return Request{}, fmt.Errorf("malformed request: path should start with /")
+	}
+	if !strings.Contains(first[2], "HTTP") {
+		return Request{}, fmt.Errorf("malformed request:first line should contain HTTP version")
+	}
+	var foundhost bool
+	var bodyStart int
+	// then we have headers, up until an empty line
+	for i := 1; i < len(lines); i ++ {
+		if lines[i] == "" { // empty line
+			bodyStart = i + 1
+			break
+		}
+		key, val, ok := strings.Cut(lines[i], ": ")
+		if !ok {
+			return Request{}, fmt.Errorf("malformed request: header %q should be of form 'key: value'", lines[i])
+		}
+		if AsTitle(key) == "Host" { //host header is required
+			foundhost = true
+		}
+		r.WithHeader(key, val)
+	}
+	end := len(lines) - 1 // recombine the body using normal newlines; skip last empty line
+	r.Body = strings.Join(lines[bodyStart:end], "\r\n")
+	if !foundhost {
+		return Request{}, fmt.Errorf("malformed request: missing Host header")
+	}
+	return r, nil
 }
--- shttp/shttp_test.go
+++ shttp/shttp_test.go
@@ -1,22 +1,20 @@
 package shttp_test
 
 import (
-	"testing"
 	"github.com/farmeroy/BackendFromTheBeginning/shttp"
+	"testing"
 )
 
 func TestTitleCaseKey(t *testing.T) {
 	for input, want := range map[string]string{
-		"foo-bar": "Foo-Bar",
+		"foo-bar":      "Foo-Bar",
 		"cONTEnt-tYPE": "Content-Type",
-		"host": "Host",
-		"Host-": "Host-",
-		"ha22-o3st": "Ha22-O3st",
+		"host":         "Host",
+		"Host-":        "Host-",
+		"ha22-o3st":    "Ha22-O3st",
 	} {
 		if got := shttp.AsTitle(input); got != want {
 			t.Errorf("TitleCaseKey(%q) = %q, want %q", input, got, want)
 		}
 	}
 }
-
-
